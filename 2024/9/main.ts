5e5235ba7630ca929fc9c908e80a9951c3d92a1d
Minghe Huang <h.minghe@gmail.com>
2024-12-11 21:47:34 +0100

    update

--- 2024/9/main.ts
+++ 2024/9/main.ts
@@ -37,9 +37,6 @@ export const checksum = (s: string): number => {
   const lst = s.split('').map(d => parseInt(d, 10));
 
   const decoded = decodeDiskMap(lst);
-  console.warn('+++');
-  console.warn(decoded.slice(0, 100));
-  console.warn('+++');
   const rearranged = freeDisk(decoded);
   // console.warn(rearranged);
   return rearranged.reduce((acc, cur, index) => {
@@ -50,10 +47,44 @@ export const checksum = (s: string): number => {
   }, 0);
 };
 
+const findFree = (pointers: any[], len: number): number[] => {
+  let start = 0;
+  let end = 0;
+  while (start < pointers.length - 1) {
+    if (pointers[start] !== '.' && pointers[end] !== '.') {
+      start++;
+      end++;
+    } else {
+      if (pointers[end] === '.') {
+        end++;
+      } else {
+        if (end - start >= len) {
+          return [start, end - 1];
+        } else {
+          start = end;
+        }
+      }
+    }
+  }
+  return [];
+};
+
+const move = (files: number[], frees: number[], pointers: any[]) => {
+  if (files[1] - files[0] >= frees[1] - frees[0]) {
+    return;
+  }
+
+  for (let i = 0; i <= files[1] - files[0]; i++) {
+    console.warn('-->', i);
+    pointers[frees[0] + i] = pointers[files[0] + i];
+    pointers[files[0] + i] = '.';
+  }
+};
+
 const moveFile = (fileStart: number, fileEnd: number, pointers: any[]) => {
   let start = 0;
   let end = 0;
-  while (start < pointers.length) {
+  while (end <= fileEnd) {
     if (pointers[start] !== '.') {
       start++;
       end++;
@@ -63,7 +94,6 @@ const moveFile = (fileStart: number, fileEnd: number, pointers: any[]) => {
       }
       if (pointers[end] !== '.') {
         if (end - start >= fileStart - fileEnd && end <= fileEnd) {
-          // [ 2, 5 ] [ 39, 41 ]
           // console.warn('move', [start, end], [fileEnd, fileStart], pointers);
           for (let i = 0; i < fileStart - fileEnd; i++) {
             console.warn(i, start + i);
@@ -84,6 +114,8 @@ export const freeDisk2 = (pointers: any[]) => {
   let fileStart = pointers.length - 1;
   let fileEnd = pointers.length - 1;
 
+  const moved: Record<string, boolean> = {};
+
   while (fileStart > 0) {
     if (pointers[fileStart] === '.') {
       fileStart--;
@@ -92,8 +124,21 @@ export const freeDisk2 = (pointers: any[]) => {
       if (pointers[fileEnd] === pointers[fileStart]) {
         fileEnd--;
       } else {
-        // console.warn('found', fileStart, fileEnd, pointers);
-        moveFile(fileStart, fileEnd, pointers);
+        console.warn('found files', [fileStart, fileEnd + 1]);
+        const frees = findFree(pointers, fileStart - fileEnd);
+        console.warn('found frees', frees);
+        if (
+          !moved[pointers[fileStart]] &&
+          frees.length > 0 &&
+          frees[1] < fileEnd + 1
+        ) {
+          console.warn('before ->', pointers);
+          move([fileEnd + 1, fileStart], frees, pointers);
+          moved[pointers[fileStart]] = true;
+          console.warn('after ->', pointers);
+        } else {
+          console.warn('skip', pointers[fileStart]);
+        }
         fileStart = fileEnd;
       }
     }
